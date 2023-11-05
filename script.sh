3c7654336008e63f68c2d621b2b172be1474a6f1
plugg1N <bbulkaskoricey87@gmail.com>
2023-11-05 17:11:12 +0300

    sapphire auto commit

--- script.sh
+++ script.sh
@@ -1,18 +1,40 @@
 #!/bin/bash
 
+
+## Sapphire was created by:
+## Zhamkov Nikita Dmitrievich
+
+## GitHub: https://github.com/plugg1N
+
+
+## How to use?:
+# 1. Initialize a Git repository in a folder
+# 2. Create a file called "sapphire.cfg"
+# 3. Fill a file with needed info:
+#     - link=(link to github repo)
+#     - message=(commit message)
+#     - branch=(branch to push to)
+# 4. Run command "sapphire" to push refs
+
 sapphire_file="sapphire.cfg"
 
+
+# Check of sapphire cfg is in the git directory
 if [ ! -f "$sapphire_file" ]; then
     echo "The 'sapphire.cfg' file does not exist in directory."
     exit 1
 fi
 
 
+# Get info from .cfg
 source $sapphire_file
 
 message=${message:-"sapphire auto commit"}
 branch=${branch:-"main"}
 
+
+
+# Check if git is initialized
 init_git() {
     if [ ! -d ".git" ]; then
 	echo "This directory is not a Git repository. Run 'git init'"
@@ -20,11 +42,13 @@ init_git() {
     fi
 }
 
+# Add origin
 add_remote_origin() {
     git remote add origin $link
 }
 
 
+# Create .gitignore and ignore .cfg file
 create_gitignore() {
 
     if ! grep -Fxq "sapphire.cfg" .gitignore 2>/dev/null; then
@@ -34,6 +58,7 @@ create_gitignore() {
 }
 
 
+# Run the script
 sapphire-run() {
     init_git
     add_remote_origin
@@ -45,4 +70,4 @@ sapphire-run() {
 }
 
 sapphire-run
-echo -e "\nThank you for using Sapphire!\nEverything was pushed successfully!"
+echo -e "\nThank you for using Sapphire!\nEverything was pushed successfully!\nMade by: plugg1N (GitHub)"
